a05d00857b8b79c605f68bc0ad3c9923cff76ee7
IvanSixSixSix <ivanpanov.95@mail.ru>
2022-02-01 09:13:04 +0300

    remove comments fix celectors

--- src/test/java/gure/IvanPanov/SearchSelenideInGithub.java
+++ src/test/java/gure/IvanPanov/SearchSelenideInGithub.java
@@ -10,20 +10,15 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class SearchSelenideInGithub {
     @Test
-    void FindSelenideRepossitoryInGithub(){
+    void findSelenideRepossitoryInGithub(){
         //Открыть сраницу github
         open("https://github.com");
-        //Ввести в поле Selenide нажать Enter
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-        //Нажать линк первого результата поиска
-        $$("ul.repo-list li").first().$("a").click();//ul - "unordered list" ‒ «неупорядоченный список» (ul class = "repo-list"),
-        // li - "list item" - элемент списка, "$$" - ищут массив элементов
-        //sleep(5000);
-         //Check: в заголовке встречается selenide/selenide
+        $("ul.repo-list li").$("a").click();
         $("h1").shouldHave(text("selenide\n" +
                 "/\n" +
                 "selenide"));
-        $("[id=wiki-tab]").click();
+        $("#wiki-tab").click();
         $(byText("Soft assertions")).click();
         $(".markdown-body").shouldHave(text("JUnit5 extension"));
     }
